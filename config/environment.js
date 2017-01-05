ee863fcf211124ed9b684e87bf39bf48aa8e44a9
Josh McArthur <joshua.mcarthur@gmail.com>
2017-01-06 11:24:55 +1300

    Add Firebase credentials

--- config/environment.js
+++ config/environment.js
@@ -19,7 +19,12 @@ module.exports = function(environment) {
     },
 
     zxingEndpoint: "zxing://scan/",
-    untappdAccessToken: process.env.UNTAPPD_ACCESS_TOKEN
+    untappdAccessToken: process.env.UNTAPPD_ACCESS_TOKEN,
+    firebaseConfig: {
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      databaseURL: process.env.FIREBASE_DATABASE_URL
+    }
   };
 
   if (environment === 'development') {
