bfd39a9591cbad011b43487d8de3de8bdb081c17
Josh McArthur <joshua.mcarthur@gmail.com>
2017-01-06 18:00:05 +1300

    Allow for subdirectory in zxing return URL

--- app/routes/checkin/start.js
+++ app/routes/checkin/start.js
@@ -4,8 +4,9 @@ import ENV from "beer-fridge/config/environment";
 export default Ember.Route.extend({
   actions: {
     scan() {
+      const currentHostAndPath = `${window.location.protocol}//${window.location.host}${window.location.pathname}`;
       const params = Ember.$.param({
-        ret: `${window.location.origin}${this.router.generate('checkin', '{CODE}')}`,
+        ret: `${currentHostAndPath}${this.router.generate('checkin', '{CODE}')}`,
         SCAN_FORMATS: 'EAN_13,UPC_A'
       });
 
