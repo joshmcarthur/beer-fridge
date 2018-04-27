5172aba67777b6e82e9757535c00ceb435f59c7b
Josh McArthur <joshua.mcarthur@gmail.com>
2018-04-27 14:48:20 +1200

    Move Firebase config settings to build settings
    These aren't secret per se, since it's JS, but my preference is to
    decouple config from implementation

--- src/base.js
+++ src/base.js
@@ -2,9 +2,9 @@ import Rebase from "re-base";
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyAYc_bUuVSffHfOUhOlLahHZCu54C8a_8U",
-  databaseURL: "https://sudojosh-beer-fridge.firebaseio.com/",
-  authDomain: "sudojosh-beer-fridge.firebaseapp.com"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN
 });
 
 const base = Rebase.createClass(firebaseApp.database());
