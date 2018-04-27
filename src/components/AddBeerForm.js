9c867fbb34eba95fc62dd3cd28dd934fc0bdbfba
Josh McArthur <joshua.mcarthur@gmail.com>
2018-04-27 14:47:39 +1200

    Implement add beer form

--- src/components/AddBeerForm.js
+++ src/components/AddBeerForm.js
@@ -1,11 +1,22 @@
 import React from "react";
 import queryString from "query-string";
+import BeerCard, { styles } from "./BeerCard";
+import ProgressIndicator from "./ProgressIndicator";
+import TextField from "material-ui/TextField";
+import RaisedButton from "material-ui/RaisedButton";
+
+const buttonStyle = {
+  marginRight: "0.5rem"
+};
 
 class AddBeerForm extends React.Component {
+  accessToken = process.env.REACT_APP_UNTAPPD_ACCESS_TOKEN;
+  quantityRef = React.createRef();
   state = {
-    record: {
-      quantity: 1
-    }
+    loaded: false,
+    beer: null,
+    brewery: null,
+    quantity: 1
   };
   componentDidMount() {
     // 1. Fetch the beer info from Untappd
@@ -18,21 +29,65 @@ class AddBeerForm extends React.Component {
     this.lookupBeer(this.props.location.queryParams.barcode);
   }
   lookupBeer(barcode) {
-    alert(barcode);
     const url = `https://api.untappd.com/v4/beer/checkbarcodemultiple?upc=${barcode}&access_token=${
       this.accessToken
     }`;
     fetch(url)
       .then(data => data.json())
       .then(json => {
-        if (!json.response.items.length) {
-          throw `${barcode} was not found on Untappd!`;
-        }
-        console.log(json.response.items);
+        if (!json.response || !json.response.items) return;
+        return json.response.items[0];
+      })
+      .then(item => {
+        if (!item) throw new Error(`${barcode} was not found on Untappd!`);
+        this.setState({ loaded: true, beer: item.beer, brewery: item.brewery });
       });
   }
+  doCancel = e => {
+    if (e) e.preventDefault();
+    this.props.history.push("/");
+  };
+  doSave = e => {
+    e.preventDefault();
+    this.props.save({
+      beer: this.state.beer,
+      brewery: this.state.brewery,
+      quantity: parseInt(this.quantityRef.current.input.value, 10)
+    });
+    this.doCancel();
+  };
   render() {
-    return <div>Oh hey</div>;
+    if (!this.state.loaded) return <ProgressIndicator />;
+    return (
+      <form onSubmit={e => this.doSave(e)}>
+        <BeerCard
+          beer={this.state.beer}
+          brewery={this.state.brewery}
+          expanded={true}
+          expandable={false}
+        />
+        <div style={styles.card}>
+          <TextField
+            floatingLabelText="Quantity"
+            defaultValue={this.state.quantity}
+            ref={this.quantityRef}
+            type="number"
+          />
+          <br />
+          <RaisedButton
+            style={buttonStyle}
+            primary={true}
+            type="submit"
+            label="Add this Beer"
+          />
+          <RaisedButton
+            style={buttonStyle}
+            onClick={e => this.doCancel(e)}
+            label="Cancel"
+          />
+        </div>
+      </form>
+    );
   }
 }
 
